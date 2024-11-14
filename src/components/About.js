bd3a755b1199c86dcb067cb98beea896561061c9
h-hritik <112206616+h-hritik@users.noreply.github.com>
2024-11-14 03:08:10 +0000

    Your commit message

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import pdf from '../documents/Hritik_Maurya_Resume_Final.pdf';
-import passImage from '../images/pass02.JPG';
-import './About.css';
+import React from "react";
+import pdf from "../documents/resume... U.pdf";
+import passImage from "../images/pass02.JPG";
+import "./About.css";
 
 function About() {
   return (
@@ -13,13 +13,18 @@ function About() {
               <h2>About Me</h2>
               <p>
                 <i>
-
-                Hi, <strong>I'm Hritik.</strong> I'm passionate about web development and enjoy creating intuitive and dynamic web experiences.
+                  Hi, <strong>I'm Hritik.</strong> I'm passionate about web
+                  development and enjoy creating intuitive and dynamic web
+                  experiences.
                 </i>
               </p>
             </div>
             <div className="col-md-6 text-center">
-              <img src={passImage} alt="Hritik Maurya" className="img-fluid rounded-circle"/>
+              <img
+                src={passImage}
+                alt="Hritik Maurya"
+                className="img-fluid rounded-circle"
+              />
             </div>
           </div>
         </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import passImage from '../images/bg15.jpg';
-import Image from '../images/React news.png';
-import passImage2 from '../images/textconverter.png';
-import './Projects.css'; // Ensure this file is created
+import React, { useState } from "react";
+import passImage from "../images/bg15.jpg";
+import Image from "../images/React news.png";
+import Image3 from "../images/image3.png";
+import passImage2 from "../images/textconverter.png";
+import "./Projects.css"; // Ensure this file is created
 
 function Projects() {
   const [showMore, setShowMore] = useState(false);
@@ -22,33 +23,101 @@ function Projects() {
                 <div className="card-body">
                   <h5 className="card-title">Dental Clinic System</h5>
                   <p className="card-text">
-                    The Dental Clinic Management System (DCMS) is a robust software solution crafted using HTML/CSS for frontend design, Bootstrap for responsive layout, Java (servlet, JSP) for backend logic, and MySQL for database management. It is tailored to streamline the daily operations of dental clinics...
-                    {showMore && <span> providing a comprehensive suite of tools to manage patient information, appointments, treatments, billing, and administrative tasks seamlessly, taking feedback within a unified platform.</span>}
+                    The Dental Clinic Management System (DCMS) is a robust
+                    software solution crafted using HTML/CSS for frontend
+                    design, Bootstrap for responsive layout, Java (servlet, JSP)
+                    for backend logic, and MySQL for database management. It is
+                    tailored to streamline the daily operations of dental
+                    clinics...
+                    {showMore && (
+                      <span>
+                        {" "}
+                        providing a comprehensive suite of tools to manage
+                        patient information, appointments, treatments, billing,
+                        and administrative tasks seamlessly, taking feedback
+                        within a unified platform.
+                      </span>
+                    )}
                   </p>
-                  <button onClick={toggleReadMore} className="btn btn-secondary">
+                  <button
+                    onClick={toggleReadMore}
+                    className="btn btn-secondary"
+                  >
                     {showMore ? "Read Less" : "Read More"}
                   </button>
-                  <a href="https://vzcwvq.csb.app/Loginhtml.html" className="btn btn-primary ml-2">View Project</a>
+                  <a
+                    href="https://vzcwvq.csb.app/Loginhtml.html"
+                    className="btn btn-primary ml-2"
+                  >
+                    View Project
+                  </a>
                 </div>
               </div>
             </div>
             <div className="col-md-4">
               <div className="card shadow project-card my-2">
-                <img src={passImage2} className="card-img-top" alt="Project 2" />
+                <img
+                  src={passImage2}
+                  className="card-img-top"
+                  alt="Project 2"
+                />
                 <div className="card-body">
                   <h5 className="card-title">Text Converter</h5>
-                  <p className="card-text">Text converter - we can perform operations on text like uppercase, lowercase, color change. The application is built in React.</p>
-                  <a href="https://text-converter-rouge.vercel.app/" className="btn btn-primary">View Project</a>
+                  <p className="card-text">
+                    Text converter - we can perform operations on text like
+                    uppercase, lowercase, color change. The application is built
+                    in React.
+                  </p>
+                  <a
+                    href="https://text-converter-rouge.vercel.app/"
+                    className="btn btn-primary"
+                  >
+                    View Project
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card shadow project-card my-2">
+                <img
+                  src={Image3}
+                  className="card-img-top"
+                  alt="Project 3"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">iNoteBook app</h5>
+                  <p className="card-text">
+                    iNoteBook is an application where we can create multiple
+                    notes with proper font editing and notes are stored on a
+                    MongoDB server. We can create, update, and delete notes, and
+                    we have a login page with proper authentication. The
+                    application is built in React, Node.js, and MongoDB.
+                  </p>
+                  <a
+                    href="https://i-note-book-app-nine.vercel.app/"
+                    className="btn btn-primary"
+                  >
+                    View Project
+                  </a>
                 </div>
               </div>
             </div>
             <div className="col-md-4 my-3">
               <div className="card shadow project-card my-2">
-                <img src={Image} className="card-img-top" alt="Project 3" />
+                <img src={Image} className="card-img-top" alt="Project 4" />
                 <div className="card-body">
                   <h5 className="card-title">React News App</h5>
-                  <p className="card-text">The website fetches news with the help of APIs, and you can choose multiple fields like science, entertainment, general, sports, etc. The application is built in React.</p>
-                  <a href="https://react-news-aihjjuth9-hritik-mauryas-projects.vercel.app/" className="btn btn-primary">View Project</a>
+                  <p className="card-text">
+                    The website fetches news with the help of APIs, and you can
+                    choose multiple fields like science, entertainment, general,
+                    sports, etc. The application is built in React.
+                  </p>
+                  <a
+                    href="https://react-news-aihjjuth9-hritik-mauryas-projects.vercel.app/"
+                    className="btn btn-primary"
+                  >
+                    View Project
+                  </a>
                 </div>
               </div>
             </div>
